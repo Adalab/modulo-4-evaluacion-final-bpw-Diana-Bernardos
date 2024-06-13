2fc7ea6e25826c4d8ed84bada3cff74d3f454504
Diana Bernardos <dianilla75@gmail.com>
2024-06-13 17:59:29 +0200

    README

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5001;
 
 api.listen(PORT, () => {
   console.log(`Server running in port : http://localhost:${PORT}`);
-  console.log(process.env.DB_NAME);
+
 });
 
 //conexion con la BD
@@ -33,21 +33,23 @@ async function conexion () {
     return conn;
    } 
    catch (error) {
+
     console.log (error);
     
   }
 };
   
-conexion();
+//conexion();
 
 // Endpoint 1 insertar un nuevo registro 
+
 api.post("/facecream", async (req, res) =>{
   try {
       const conn = await conexion();
     const { tipe, ingredient, descr } = req.body;
   
     const sqlInsert =
-      'insert into facecream (tipe, ingredient, descr) values(?,?,?)';
+      'INSERT INTO facecream (tipe, ingredient, descr) values(?,?,?)';
     const [newData] = await conn.query(sqlInsert, [
         tipe,
         ingredient,
@@ -73,7 +75,7 @@ api.get("/facecream", async (req,res)=>{
     try {
         const conn = await conexion();
 
-        const select= "select * from facecream";
+        const select= "SELECT * FROM facecream";
         const [result]= await conn.query(select);
         res.status(200).json({
             info:{count:result.length},
@@ -87,31 +89,39 @@ api.get("/facecream", async (req,res)=>{
 });
 
 //filtrar por id 
-api.put('/cream/:id', async (req, res) => {
+
+api.put('/cream/:idCream', async (req, res) => {
+  try {
     const conn = await conexion();
-    const idCream = req.params.id;
+    const idCream = req.params.idCream;
     const newData = req.body;
     const modificarSql =
-      'UPDATE facecream SET  tipe = ?, ingredient= ?, descr= ?  WHERE idCream = ?';
+      'UPDATE facecream SET tipe = ?, ingredient= ?, descr= ?  WHERE idCream = ?';
     const [result] = await conn.query(modificarSql, [
       newData.tipe,
       newData.ingredient,
       newData.descr,
       idCream,
-    ]);
+    ])
     if (result.affectedRows > 0) {
       res.status(200).json({ success: true });
     } else {
       res.status(200).json({ success: false, message: 'No existe el id ' });
     }
-    console.log(result);
-  });
+  } catch (error) {
+  console.error('Error ejecutando la consulta',error);
+  return res.status(500).json ({success :false, message : 'No exixte id'});
+}});
 
+    
+  
   // delete un registro 
-  api.delete('/facecream/:id', async (req,res)=>{
-    const conn = await conexion();
-    const idCream = req.params.id;
-    const deleteSql = 'delete from facecream WHERE id = ?';
+  
+  api.delete('/facecream/:idCream', async (req,res)=>{
+    try {
+      const conn = await conexion();
+    const idCream = req.params.idCream;
+    const deleteSql = 'DELETE FROM facecream WHERE idCream = ?';
     const[result]= await conn.query(deleteSql , [idCream]);
     if(result.affectedRows >0){
       res.status(200).json({success :true});
@@ -119,6 +129,11 @@ api.put('/cream/:id', async (req, res) => {
       res.status(200).json({success:false , message:'no existe id'});
     }
     console.log(result);
+    } catch (error) {
+      console.error('Error ejecutando la consulta',error);
+      return res.status(500).json ({success :false, message : 'Error del servidor'});
+    }
+    
   });
     
  
